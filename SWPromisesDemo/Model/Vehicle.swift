0789983b0a829686413cc37e3fba805548b14cdd
Olivier Halligon <olivier@halligon.net>
2015-02-26 02:37:51 +0100

    Implemented "Vehicle" model structure

--- SWPromisesDemo/Model/Vehicle.swift
+++ SWPromisesDemo/Model/Vehicle.swift
@@ -8,13 +8,45 @@
 
 import Foundation
 
-struct Vehicle : JSONModelObject, Printable {
+struct Vehicle : JSONModelObject {
     static var apiEndPoint: String { return "vehicles" }
     let resourceInfo: ResourceInfo
     
+    let name: String
+    let model: String
+    let vehicle_class: String
+    let manufacturer: [String]?
+    let length: Int?            // in meters
+    let cost_in_credits: Int?   // in Galactic Credits.
+    let crew: Int?
+    let passengers: Int?
+    let max_atmosphering_speed: Int?
+    let cargo_capacity: Int?    // in kilograms that this vehicle can transport.
+    let consumables: String
+    let films: [ResourceURL<Film>]
+    let pilots: [ResourceURL<Person>]
+    
     init(dict: NSDictionary) {
+        name = dict["name"] as String
+        model = dict["model"] as String
+        vehicle_class = dict["vehicle_class"] as String
+        manufacturer = parseStringList(dict["manufacturer"] as String)
+        length = (dict["length"] as String).toInt()
+        cost_in_credits = (dict["cost_in_credits"] as String).toInt()
+        crew = (dict["crew"] as String).toInt()
+        passengers = (dict["passengers"] as String).toInt()
+        max_atmosphering_speed = (dict["max_atmosphering_speed"] as String).toInt()
+        cargo_capacity = (dict["cargo_capacity"] as String).toInt()
+        consumables = dict["consumables"] as String
+        films = (dict["films"] as [String]).map { ResourceURL<Film>(url: $0) }
+        pilots = (dict["pilots"] as [String]).map { ResourceURL<Person>(url: $0) }
+        
         resourceInfo = ResourceInfo(dict: dict)
     }
+}
 
-    var description: String { return "<Vehicle #\(resourceInfo.id!)>" }
+extension Vehicle : Printable {
+    var description: String {
+        return "<Vehicle #\(resourceInfo.id!): \(name) (\(model))>"
+    }
 }
--- SWPromisesDemo/ViewController.swift
+++ SWPromisesDemo/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        fetchFirstPersonAndPrintStarships()
+//        fetchFirstPersonAndPrintVehicles()
         fetchFalconAndPrintPilotsHomeworld()
 
     }
@@ -22,16 +22,23 @@ class ViewController: UIViewController {
     private func fetchFalconAndPrintPilotsHomeworld() {
         let falcon = ResourceURL<Starship>(id: 10)
         falcon.fetch().then({ ship -> Promise<Person> in
-            println("--> Starship found: \(ship)")
-            let firstPilot = ship.pilots[0] // a ResourceURL<Pilot>
-            return firstPilot.fetch()
+            let nbPilots = countElements(ship.pilots)
+            println("--> Starship found: \(ship) (\(nbPilots) pilots)")
+            let aPilot = ship.pilots[0] // a ResourceURL<Pilot>
+            return aPilot.fetch()
         }).then({ pilot -> Void in
-            println("--> First Pilot found: \(pilot)")
+            println("--> Pilot found: \(pilot)")
             
             // Fetch all films the pilot played into
             when(pilot.films.map{$0.fetch()}).then({ films in
                 println("--> Played in films: \(films)")
             })
+            
+            // Fetch all vehicles the pilot drove
+            when(pilot.vehicles.map{$0.fetch()}).then({ vehicles in
+                println("--> Drove: \(vehicles)")
+            })
+            
             
             let homeplanet = pilot.homeworld // a ResourceURL<Planet>
             homeplanet.fetch().then({ planet in
@@ -45,16 +52,16 @@ class ViewController: UIViewController {
         })
     }
     
-    private func fetchFirstPersonAndPrintStarships() {
+    private func fetchFirstPersonAndPrintVehicles() {
         println("Fetching person #1…")
         
-        // - Given a ship, send all the requests (in parallel) to fetch all its pilots
-        //   by using map() to transform the Array<ResourceURL<Starship>> into an Array<Promise<Starship>>
+        // - Given a vehicle, send all the requests (in parallel) to fetch all its pilots
+        //   by using map() to transform the [<ResourceURL<Vehicle>] array into an [Promise<Vehicle>] array
         // - Wait for all pilot requests to finish (using when() to wait until all promises are fulfilled)
-        //   then print the starship and all its pilots.
-        func printShipWithPilots(ship: Starship) {
-            when(ship.pilots.map { $0.fetch() }).then { (pilots :[Person]) -> Void in
-                println("-- Starship: \(ship.name), \(ship.model)")
+        //   then print the vehicle and all its pilots.
+        func printVehicleWithPilots(vehicle: Vehicle) {
+            when(vehicle.pilots.map { $0.fetch() }).then { (pilots :[Person]) -> Void in
+                println("-- Vehicle: \(vehicle.name), \(vehicle.model)")
                 for pilot in pilots { println("   '-- Pilot: \(pilot)") }
             }
         }
@@ -63,9 +70,9 @@ class ViewController: UIViewController {
         ResourceURL<Person>.fetch(id: 1)
             .then({ (p: Person) -> Void in
                 println("First person found: \(p)")
-                for ship in p.starships {
+                for vehicle in p.vehicles {
                     // fetch each ship resource then print it
-                    ship.fetch().then(printShipWithPilots)
+                    vehicle.fetch().then(printVehicleWithPilots)
                 }
             })
     }
